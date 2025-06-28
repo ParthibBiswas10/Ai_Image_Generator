30f816c6af8f3f4db95174e0df70bd6c3350fd60
ParthibBiswas10 <parthiblm10@gmail.com>
2025-06-28 17:06:19 +0530

    Set up

--- ImageGen_Ai/src/components/ImageGen.jsx
+++ ImageGen_Ai/src/components/ImageGen.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 import "./ImageGen.css";
+import defaultImage from "../assets/Default.png";
 const ImageGen = () => {
   return (
-    <>
-      <div className="image-gen">hlw</div>
-      <div className="image-gen__description">
-        This is an image generator component
+    <div className="container">
+      <div className="header">
+        <span id="ImageText">Image </span>
+        <span id="GeneratorText">Generator</span>
       </div>
-    </>
+
+      <div className="image-loader">
+        <img src={defaultImage} alt="Default" className="default-image" />
+      </div>
+      <div className="wrap-input-14">
+        <input className="input" type="text" placeholder="Search Here" />
+        <span className="focus-bg"></span>
+      </div>
+    </div>
   );
 };
 
