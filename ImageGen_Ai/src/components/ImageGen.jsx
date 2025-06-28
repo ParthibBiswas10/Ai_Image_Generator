b211d1aed09ffe4fa899797e6b1933b540b05a96
ParthibBiswas10 <parthiblm10@gmail.com>
2025-06-28 17:56:32 +0530

    front update

--- ImageGen_Ai/src/components/ImageGen.jsx
+++ ImageGen_Ai/src/components/ImageGen.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import "./ImageGen.css";
 import defaultImage from "../assets/Default.png";
+const generateImage = () => {
+  console.log("Image generation triggered");
+};
 const ImageGen = () => {
   return (
     <div className="container">
@@ -16,7 +19,9 @@ const ImageGen = () => {
         <div className="wrap-input-14">
           <input className="input" type="text" placeholder="Enter Prompt" />
           <span className="focus-bg"></span>
-          <div className="btn">Search</div>
+          <div className="btn" onClick={generateImage}>
+            Search
+          </div>
         </div>
       </div>
     </div>
