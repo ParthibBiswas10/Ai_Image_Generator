501692b957f842f8aded1172fd50fff76c034d38
ParthibBiswas10 <parthiblm10@gmail.com>
2025-06-29 16:31:02 +0530

    Feature add

--- ImageGen_Ai/src/components/ImageGen.jsx
+++ ImageGen_Ai/src/components/ImageGen.jsx
@@ -1,17 +1,50 @@
-import React from "react";
-import { GoogleGenerativeAI } from "@google/generative-ai";
-import { useState, useEffect, useRef } from "react";
+import React, { useState, useRef } from "react";
+import { GoogleGenAI, Modality } from "@google/genai";
 import "./ImageGen.css";
 import defaultImage from "../assets/Default.png";
+
 const ImageGen = () => {
-  const [imagesrc, setImageSrc] = useState(defaultImage);
-  const apiKey = import.meta.env.VITE_API_KEY;
+  const [ImageSrc, setImageSrc] = useState(defaultImage);
+  const [loading, setLoading] = useState(false);
+
+  const key = import.meta.env.VITE_API_KEY;
   const inputref = useRef(null);
 
-  const generateImage = () => {
-    console.log("Image generation triggered");
-    console.log("Prompt:", inputref.current?.value);
+  const generateImage = async () => {
+    try {
+      const prompt = inputref.current.value;
+      if (!prompt) {
+        alert("Please enter a prompt to generate an image.");
+        return;
+      }
+
+      setLoading(true);
+      const api = new GoogleGenAI({ apiKey: key });
+
+      const response = await api.models.generateContent({
+        model: "gemini-2.0-flash-preview-image-generation",
+        contents: [{ text: prompt }],
+        config: {
+          responseModalities: [Modality.TEXT, Modality.IMAGE],
+        },
+      });
+
+      const parts = response.candidates[0].content.parts;
+      for (const part of parts) {
+        if (part.inlineData) {
+          const base64 = part.inlineData.data;
+          setImageSrc(`data:image/png;base64,${base64}`);
+          inputref.current.value = ""; // Clear the input field after generation
+        }
+      }
+    } catch (error) {
+      console.error("Error generating image:", error);
+      alert("Error generating image. Please try again.");
+    } finally {
+      setLoading(false);
+    }
   };
+
   return (
     <div className="container">
       <div className="header">
@@ -20,8 +53,17 @@ const ImageGen = () => {
       </div>
 
       <div className="image-loader">
-        <img src={defaultImage} alt="Default" className="default-image" />
+        <img src={ImageSrc} alt="Generated" className="generated-image" />
+        {loading && (
+          <div className="loading-container">
+            <div className="loader">Generating Image...</div>
+            <div className="loading-bar-container">
+              <div className="loading-bar"></div>
+            </div>
+          </div>
+        )}
       </div>
+
       <div className="searchbox">
         <div className="wrap-input-14">
           <input
