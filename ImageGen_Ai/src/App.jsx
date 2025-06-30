d315d96ee0aa3e81850c5326a3fa9d445bbd87dd
ParthibBiswas10 <parthiblm10@gmail.com>
2025-06-30 17:07:58 +0530

    new setting

--- ImageGen_Ai/src/App.jsx
+++ ImageGen_Ai/src/App.jsx
@@ -1,15 +1,29 @@
 import { useState } from "react";
 import "./App.css";
 import ImageGen from "./components/ImageGen";
-import DotGrid from "../effect/DotGrid/DotGrid";
+import Particles from "../effect/Particles/Particles";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div style={{ position: "relative", width: "100%", height: "100vh" }}>
-      <DotGrid />
-      <div style={{ position: "relative", zIndex: 10 }}>
+    <div
+      style={{
+        position: "relative",
+        width: "100%",
+        height: "100%",
+        overflow: "hidden",
+      }}
+    >
+      <Particles
+        particleCount={1000}
+        particleColors={["#ff69b4", "#00ff88", "#0088ff", "#ffffff"]}
+        particleSpread={15}
+        speed={1.5}
+        particleBaseSize={150}
+        moveParticlesOnHover={true}
+        alphaParticles={true}
+        particleHoverFactor={2}
+      />
+      <div style={{ position: "relative", zIndex: 100 }}>
         <ImageGen />
       </div>
     </div>
