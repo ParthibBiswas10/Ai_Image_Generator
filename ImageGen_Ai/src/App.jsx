efd9bffe2f3a6b5ea18c2d97e51731c96ba5d3bb
ParthibBiswas10 <parthiblm10@gmail.com>
2025-06-30 11:55:09 +0530

    Effect Set

--- ImageGen_Ai/src/App.jsx
+++ ImageGen_Ai/src/App.jsx
@@ -1,14 +1,18 @@
 import { useState } from "react";
 import "./App.css";
 import ImageGen from "./components/ImageGen";
+import DotGrid from "../effect/DotGrid/DotGrid";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <ImageGen />
-    </>
+    <div style={{ position: "relative", width: "100%", height: "100vh" }}>
+      <DotGrid />
+      <div style={{ position: "relative", zIndex: 10 }}>
+        <ImageGen />
+      </div>
+    </div>
   );
 }
 
