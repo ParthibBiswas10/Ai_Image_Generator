94b4407a3ad1476aafb4cf3a407e46c4d866659a
ParthibBiswas10 <parthiblm10@gmail.com>
2025-07-01 22:37:52 +0530

    new setting

--- ImageGen_Ai/src/App.jsx
+++ ImageGen_Ai/src/App.jsx
@@ -2,19 +2,19 @@ import { useState } from "react";
 import "./App.css";
 import ImageGen from "./components/ImageGen";
 import Particles from "../effect/Particles/Particles";
-
+import Footer from "./components/footer";
 function App() {
   return (
     <div
       style={{
         position: "relative",
         width: "100%",
-        height: "100%",
+        height: "100vh",
         overflow: "hidden",
       }}
     >
       <Particles
-        particleCount={1000}
+        particleCount={900}
         particleColors={["#ff69b4", "#00ff88", "#0088ff", "#ffffff"]}
         particleSpread={15}
         speed={1.5}
@@ -26,6 +26,7 @@ function App() {
       <div style={{ position: "relative", zIndex: 100 }}>
         <ImageGen />
       </div>
+      <Footer />
     </div>
   );
 }
---
+++ ImageGen_Ai/src/components/footer.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+const footer = () => {
+  return (
+    <>
+      <div class="patterns">
+        <svg width="100%" height="30px">
+          <defs>
+            <style>
+              @import url("https://fonts.googleapis.com/css?
+              family=Lora:400,400i,700,700i");
+            </style>
+          </defs>
+          <text x="50%" y="100%" text-anchor="middle">
+            CONNECT ME WITH
+          </text>
+        </svg>
+      </div>
+      <div class="social-item">
+        <a href="https://www.facebook.com/parthib.biswas.31924">
+          <i class="fa-brands fa-facebook"></i>
+        </a>
+        <a href="https://www.instagram.com/messian_muggle10/?hl=en">
+          <i class="fa-brands fa-instagram"></i>
+        </a>
+        <a href="https://www.linkedin.com/in/parthib-biswas-79b06029b/">
+          <i class="fa-brands fa-linkedin"></i>
+        </a>
+        <a href="https://github.com/ParthibBiswas10">
+          <i class="fa-brands fa-github"></i>
+        </a>
+        <a href="https://x.com/ParthibMessi10">
+          <i class="fa-brands fa-x-twitter"></i>
+        </a>
+        <a href="https://in.pinterest.com/parthiblm10/">
+          <i class="fa-brands fa-pinterest"></i>
+        </a>
+      </div>
+    </>
+  );
+};
+
+export default footer;
